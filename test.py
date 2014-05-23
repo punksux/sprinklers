a1d741ff2b10ac432c275fd2781118bca29d27b1
punksux <punksux2.0@gmail.com>
2014-05-23 15:05:49 -0600

    fix negative start time
    weather test to not use all api calls

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 #Settings 
 days_between = .5
-time_to_start = '12:00:00'
+time_to_start = '15:04:00'
 times = [40,30,30] 
 on = True
 zones = [7,11,13]
 location = "84123"
 on_pi=False
+weather_test = 1.0
 
 rain = 0.00
 day = 86400
@@ -48,15 +49,18 @@ else:
 import json
 
 def check_weather():
-    if on_pi:
-        f = urllib2.urlopen('http://api.wunderground.com/api/c5e9d80d2269cb64/geolookup/conditions/q/%s.json' %(location))
+    if weather_test == 0:
+        if on_pi:
+            f = urllib2.urlopen('http://api.wunderground.com/api/c5e9d80d2269cb64/geolookup/conditions/q/%s.json' %(location))
+        else:
+            f = urlopen('http://api.wunderground.com/api/c5e9d80d2269cb64/geolookup/conditions/q/%s.json' %(location))
+        json_string = f.read()
+        parsed_json = json.loads(json_string.decode("utf8"))
+        global rain
+        rain = parsed_json['current_observation']['precip_today_in']
+        f.close()
     else:
-        f = urlopen('http://api.wunderground.com/api/c5e9d80d2269cb64/geolookup/conditions/q/%s.json' %(location))
-    json_string = f.read()
-    parsed_json = json.loads(json_string.decode("utf8"))
-    global rain
-    rain = parsed_json['current_observation']['precip_today_in']
-    f.close()
+        rain = str(weather_test)
 
 #Set up GPIO
 from datetime import datetime
@@ -76,59 +80,59 @@ now = datetime.now()
 print (now)
 
 splits = time_to_start.split(":")
-time_to_start = datetime.now().replace(hour=int(splits[0]), minute=int(splits[1]))
-print (time_to_start)
+time_to_start = datetime.now().replace(hour=int(splits[0]), minute=int(splits[1]), second=00, microsecond=0)
 
-run_at = time_to_start - now 
-print (run_at)
+run_at = time_to_start - now
+if run_at.total_seconds() < 0:
+    print (time_to_start.replace(day=time_to_start.day+1))
+    run_at = (time_to_start.replace(day=time_to_start.day+1)) - now
 
 delay = run_at.total_seconds()
-print ('Starting in ' + str(delay/60) + 'min')
+print ('Starting in ' + str(delay/60) + ' min')
 
 def hello():
     global rt
     rt.stop()
     check_weather()
-    print(rain)
+    print(rain + " inches of rain")
     if float(rain) > 0.125:
         print ('Canceling for rain')
-        rt = RepeatedTimer(day, hello2)
+        rt = RepeatedTimer(day, hello)
     else:
         for i in range(0,len(times)):
             print ('%s - Zone %s on: %s min.' %(str(datetime.now()),str(i+1),str(times[i])))
             if on_pi:
                 GPIO.output(zones[i],True)
-            time.sleep(times[i]*60)
+            time.sleep(times[i])
             print ('Zone %s off.' %(str(i+1)))
             if on_pi:
                 GPIO.output(zones[i],False)
             time.sleep(1)
-        print ("Starting Daily...")
-        rt = RepeatedTimer(seconds_between, hello2) 
+        #print ("Starting Daily...")
+        rt = RepeatedTimer(seconds_between, hello) 
 
-def hello2():
-    global rt
-    rt.stop()
-    check_weather()
-    if float(rain) > 0.25:
-        print ('Canceling for rain')
-        rt = RepeatedTimer(day, hello2)
-    else:
-        for i in range(0,len(times)):
-            print ('%s - Zone %s on: %s min.' %(str(datetime.now()),str(i+1),str(times[i])))
-            if on_pi:
-             GPIO.output(zones[i],True)
-            time.sleep(times[i]*60)
-            print ('Zone %s off.' %(str(i+1)))
-            if on_pi:
-                GPIO.output(zones[i],False)
-            time.sleep(1)
-        rt = RepeatedTimer(seconds_between, hello2)
+##def hello2():
+##    global rt
+##    rt.stop()
+##    check_weather()
+##    if float(rain) > 0.25:
+##        print ('Canceling for rain')
+##        rt = RepeatedTimer(day, hello2)
+##    else:
+##        for i in range(0,len(times)):
+##            print ('%s - Zone %s on: %s min.' %(str(datetime.now()),str(i+1),str(times[i])))
+##            if on_pi:
+##             GPIO.output(zones[i],True)
+##            time.sleep(times[i]*60)
+##            print ('Zone %s off.' %(str(i+1)))
+##            if on_pi:
+##                GPIO.output(zones[i],False)
+##            time.sleep(1)
+##        rt = RepeatedTimer(seconds_between, hello2)
         
 print ("Starting First Time...")
 global rt
 rt = RepeatedTimer(delay, hello) # it auto-starts, no need of rt.start()
-
     
 try:
     sleep(300000) # your long-running job goes here...
