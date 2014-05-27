f7a50ecd979c5610a768b4672ccc7d3bd3e02be9
punksux <punksux2.0@gmail.com>
2014-05-27 12:51:13 -0600

    flask test

---
+++ flaskTest.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+import time
+from threading import Thread
+
+app = Flask(__name__)
+
+no = "Poo"
+
+def print_stuff():
+    while True:
+        print (no)
+        time.sleep (1)
+
+
+thread = Thread(target = print_stuff)
+thread.start()
+
+@app.route('/')
+def my_form():
+    return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+
+    text = request.form['text']
+    global no
+    no = str(text)
+    processed_text = text.upper()
+    return processed_text
+
+if __name__ == '__main__':
+    app.run()
+
+def change(text):
+    global no
+    no = str(text)
+
+
+
